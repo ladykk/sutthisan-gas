417e1c5984e339a717a9c5b0559c710c0dce9d22
Rattapon Apiratjit <krtp.apirat@gmail.com>
2023-12-06 16:09:22 +0700

    fix deploy bug.

---
+++ drizzle-env.mjs
@@ -0,0 +1,9 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+export const drizzleEnv = createEnv({
+  server: {
+    DATABASE_URL: z.string().url(),
+  },
+  runtimeEnv: process.env,
+  emptyStringAsUndefined: true,
+});
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,4 @@
-import { drizzleEnv } from "@/env.mjs";
+import { drizzleEnv } from "./drizzle-env.mjs";
 import type { Config } from "drizzle-kit";
 export default {
   schema: "./src/server/db/schema.ts",
